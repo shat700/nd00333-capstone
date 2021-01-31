7b163b234f90d9331e506f61c582cfe6be43ded4
shat700 <60496195+shat700@users.noreply.github.com>
2021-01-31 21:25:28 +0000

    Add files via upload

--- starter_file/scoring.py
+++ starter_file/scoring.py
@@ -1,13 +1,13 @@
 import json
 import numpy as np
 import os
-from sklearn.externals import joblib
-
+import pickle
 
 def init():
     global model
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.joblib')
-    model = joblib.load(model_path)
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'automl_model.pkl')
+    with open(model_path, 'rb') as f:
+        model = pickle.load(f)
 
 def run(data):
     try:
