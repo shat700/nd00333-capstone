1ff9df2423c4cab2bd25c313d830f42f549cf564
shat700 <60496195+shat700@users.noreply.github.com>
2021-01-31 21:24:30 +0000

    Add files via upload

---
+++ scoring.py
@@ -0,0 +1,20 @@
+import json
+import numpy as np
+import os
+import pickle
+
+def init():
+    global model
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'automl_model.pkl')
+    with open(model_path, 'rb') as f:
+        model = pickle.load(f)
+
+def run(data):
+    try:
+        data = np.array(json.loads(data))
+        result = model.predict(data)
+        # You can return any data type, as long as it is JSON serializable.
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+     
